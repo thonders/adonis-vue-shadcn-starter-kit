3c541d2fd3835c59e4e1f1a135ab7dd6b3593eee
Thonder <i@thond.re>
2025-08-21 14:18:21 +0700

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import { getDirname } from '@adonisjs/core/helpers'
+import inertia from '@adonisjs/inertia/client'
+import vue from '@vitejs/plugin-vue'
+import adonisjs from '@adonisjs/vite/client'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.ts' } }),
+    vue(),
+    adonisjs({ entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge'] }),
+  ],
+
+  /**
+   * Define aliases for importing modules from
+   * your frontend code
+   */
+  resolve: {
+    alias: {
+      '~/': `${getDirname(import.meta.url)}/inertia/`,
+      '@/': `${getDirname(import.meta.url)}/inertia/`,
+    },
+  },
+})
